8737429ba3ecf03f79029f3e5be7cede05db943b
SanjanaR2309 <sanjublr923@gmail.com>
2024-08-22 21:22:27 +0530

    Add files via upload

---
+++ queue.py
@@ -0,0 +1,46 @@
+"""
+Description: Program to implement queue
+Author: Sanjana R
+Date: 22/08/2024
+Place: Bangalore
+"""
+
+class Queue:
+    def __init__(self):
+        # Initialize an empty list to store queue items
+        self.queue = []
+
+    def enqueue(self, item):
+        # Add the item to the back of the queue
+        self.queue.append(item)
+
+    def dequeue(self):
+        # Remove and return the front item from the queue
+        # (The first item added)
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            return "Queue is empty"
+
+    def is_empty(self):
+        # Check if the queue is empty
+        return len(self.queue) == 0
+
+    def __repr__(self):
+        # Return a string representation of the queue
+        return f"{self.queue}"
+
+# Example usage:
+queue = Queue()
+a = int(input("Enter the number of elements in the queue: "))
+for i in range(a):
+    b = int(input('Enter the element to the queue: '))
+    queue.enqueue(b)
+
+print("The queue elements are:", queue)
+
+if not queue.is_empty():
+    print("Element popped is", queue.dequeue()) 
+    print("Element popped is", queue.dequeue())  
+else:
+    print("Queue is empty")
---
+++ stack.py
@@ -0,0 +1,45 @@
+"""
+Description: Program to implement stack
+Author: Sanjana R
+Date: 22/08/2024
+Place: Bangalore
+"""
+class Stack:
+    def __init__(self):
+        # Initialize an empty list to store stack items
+        self.stack = []
+
+    def push(self, item):
+        # Add the item to the top of the stack
+        self.stack.append(item)
+
+    def pop(self):
+        # Remove and return the top item from the stack
+        # (The last item added)
+        if not self.is_empty():
+            return self.stack.pop()
+        else:
+            return "Stack is empty"
+
+    def is_empty(self):
+        # Check if the stack is empty
+        return len(self.stack) == 0
+
+    def __repr__(self):
+        # Return a string representation of the stack
+        return f"{self.stack}"
+
+# Example usage:
+stack = Stack()
+a = int(input("Enter the number of elements in the stack: "))
+for i in range(a):
+    b = int(input("Enter the element to stack: ")) 
+    stack.push(b) 
+
+print("The elements in stack are:", stack) 
+
+if not stack.is_empty():
+    print("Element popped from stack:", stack.pop())
+    print("Element popped from stack:", stack.pop())
+else:
+    print("Stack is empty")
